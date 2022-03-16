a21ca7d3d5cafef811687d424a54b89113aa96a7
jesschan29 <92983883+jesschan29@users.noreply.github.com>
2022-03-16 19:12:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import pandas as pd 
 import streamlit as st 
-# st.set_page_config(page_title="Hello World")
+st.set_page_config(page_title="Hello World")
 
 st.title("Exploring Netflix Movies & Tv Shows")
